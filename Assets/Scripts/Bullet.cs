e155950da9b369f5e278413841754c34c1554f4a
FabianCz95 <fabiancruz9504@gmail.com>
2020-12-12 05:07:51 -0600

    Proyecto Final

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 2f;
+    public float velocity = 2f;
+    public Vector2 direction;
+
+    public float livingTime = 2f;
+    public Color initialColor = Color.white;
+    public Color finalColor;
+
+    private SpriteRenderer _renderer;
+    private float _startingTime;
+    private Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Guarda el tiempo inicial
+        _startingTime = Time.time;
+
+        //Destroy the bullet after some time
+        Destroy(this.gameObject, livingTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Cambiar color de bala
+        float _timeSinceStarted = Time.time - _startingTime;
+        float _percentageCompleted = _timeSinceStarted / livingTime;
+
+        _renderer.color = Color.Lerp(initialColor, finalColor, _percentageCompleted);
+    }
+
+    private void FixedUpdate()   
+    {
+        //Move Object
+        Vector2 movement = direction.normalized * speed;
+        _rigidbody.velocity = movement;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.CompareTag("Player")) {
+            Debug.Log("Encontre al player");
+        }
+    }
+}
---
+++ Assets/Scripts/TestingKeys.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. Mouse
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("button presed");
+        }
+        if (Input.GetMouseButton(0)) {
+            Debug.Log("button is pressed");
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            Debug.Log("button released");
+        }
+
+        //1.1 GetMouseButton(0) se puede cambiar por 1
+        // 0 = boton izquierdo del mouse
+        // 1 = boton derecho del mouse
+
+        //Generar eventon con teclado
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Usando KeyCode. Puedes usar esto para hacer que el jugador salte");
+        }
+
+        if(Input.GetButtonDown("Jump")){
+            Debug.Log("Usando Jump. puedes hacer saltar al jugador");
+        }
+
+        //3.- axis de movimientos
+        float horizontal = Input.GetAxis("Horizontal"); // -1 a 1
+        float vertical = Input.GetAxis("Vertical"); // -1 to 1
+
+        if (horizontal < 0f || horizontal > 0f) {
+            Debug.Log("Horizontal axis is" + horizontal);
+        }
+        if (vertical < 0f || vertical > 0f) {
+            Debug.Log("Vertical axis is" + vertical);
+        }
+    }
+}
